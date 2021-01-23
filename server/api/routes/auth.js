b962e6ed1e36ddf282264ed4b6a651cbcdd924b7
clabroche <corentin.labroche@sailendra.fr>
2021-01-23 01:50:33 +0100

    fix(auth): crypt password with bcrypt

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -1,12 +1,13 @@
 const app = require('express').Router()
+const bcrypt = require('bcrypt')
 /**
  * Check if user has type the correct password to get access to front
  * (Very bad: we need to hash password with bcrypt, send a token to user and validate all others routes with this token to protect them)
  * @express-body {string} password
  */
-app.post('/auth', (req, res) => {
+app.post('/auth', async(req, res) => {
   const password = process.env.PASSWORD
-  if (password === req.body.password) {
+  if (await bcrypt.compare(req.body.password, password)) {
     return res.json(true)
   }
   res.json(false)
